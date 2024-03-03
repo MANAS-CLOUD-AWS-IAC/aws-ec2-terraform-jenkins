b4990b9ffecb4e02dbe6ae32eb6b486139f91263
Manas Dash <manas.d2@gmail.com>
2024-03-03 15:44:22 +0530

    test

---
+++ ec2.tf
@@ -0,0 +1,144 @@
+# configured aws provider with proper credentials
+provider "aws" {
+  region = "us-east-1"
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# create default vpc if one does not exit
+resource "aws_default_vpc" "default_vpc" {
+
+  tags = {
+    Name = "default vpc"
+  }
+}
+
+# use data source to get all avalablility zones in region
+data "aws_availability_zones" "available_zones" {}
+
+# create default subnet if one does not exit
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
+
+  tags = {
+    Name = "default subnet"
+  }
+}
+
+# create security group for the ec2 instance
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2 security group"
+  description = "allow access on ports 8080 and 22"
+  vpc_id      = aws_default_vpc.default_vpc.id
+
+  # allow access on port 8080
+  ingress {
+    description = "http proxy access"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # allow access on port 22
+  ingress {
+    description = "ssh access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins server security group"
+  }
+}
+
+# Create a new S3 bucket for Jenkins artifacts
+/*The aws_s3_bucket resource defines an S3 bucket that will be used to store Jenkins artifacts. The bucket will be named jenkins-
+artifacts-s3bucket-jason2023 and will have a private ACL.
+*/
+
+resource "aws_s3_bucket" "jenkins_artifacts_bucket" {
+  bucket = "jenkins-week20-s3bucket-dibaalakariimiete2023" # Set a unique bucket name
+# Deny public access to the S3 bucket
+  #acl = "private"
+}
+
+# use data source to get a registered amazon linux 2 ami
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+# launch the ec2 instance and install website
+resource "aws_instance" "ec2_instance" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_default_subnet.default_az1.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  key_name               = "MyKeyPair1"
+  # user_data            = file("install_jenkins.sh")
+
+  tags = {
+    Name = "jenkins server"
+  }
+}
+
+# an empty resource block
+resource "null_resource" "name" {
+
+  # ssh into the ec2 instance 
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/Downloads/MyKeyPair1.pem")
+    host        = aws_instance.ec2_instance.public_ip
+  }
+
+  # copy the install_jenkins.sh file from your computer to the ec2 instance 
+  provisioner "file" {
+    source      = "install_jenkins.sh"
+    destination = "/tmp/install_jenkins.sh"
+  }
+
+  # set permissions and run the install_jenkins.sh file
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/install_jenkins.sh",
+      "sh /tmp/install_jenkins.sh",
+    ]
+  }
+
+  # wait for ec2 to be created
+  depends_on = [aws_instance.ec2_instance]
+}
+
+# output.tf
+# print the url of the jenkins server
+output "website_url" {
+  value = join("", ["http://", aws_instance.ec2_instance.public_dns, ":", "8080"])
+}
---
+++ installjenkins.sh
@@ -0,0 +1,12 @@
+#shell script for installing jenkins
+#!/bin/bash
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade -y
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo dnf install java-11-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPasswordSHOWN BELOW IS THE ENTIRE CONFIGURATION FOR THIS PROJECT. # configured aws provider with proper credentials
